888e468279a52773d3cdfb4813a1349955cc8559
Aliaksandr Hameza <otulan@example.com>
2022-10-06 21:23:17 +0300

    Delete role

--- common/src/main/java/com/noirix/repository/springdata/RolesSpringDataRepository.java
+++ common/src/main/java/com/noirix/repository/springdata/RolesSpringDataRepository.java
@@ -1,6 +1,6 @@
 package com.noirix.repository.springdata;
 
-import com.noirix.domain.Role;
+//import com.noirix.domain.Role;
 import com.noirix.domain.hibernate.HibernateRole;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -22,7 +22,7 @@ public interface RolesSpringDataRepository extends JpaRepository<HibernateRole, 
 
     @Query(value = "select r from HibernateRole r" +
             " inner join HibernateUser u on r.id = u.id ")
-    List<Role> findRolesByUserId(Long userId);
+    List<HibernateRole> findRolesByUserId(Long userId);
 
      @Query(value = "select r from HibernateRole r")
      Optional<HibernateRole> findById(Long userId);
